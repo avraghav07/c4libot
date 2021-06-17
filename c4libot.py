483f18da96f84d297671a5f12d85589fb2fda9d1
Raghav <zCalibeRz1337@gmail.com>
2021-06-17 16:10:01 +0530

    Added catspam function

--- c4libot.py
+++ c4libot.py
@@ -3,6 +3,9 @@
 #libraries
 from telegram.ext import MessageHandler, Filters, CommandHandler, Updater
 import logging
+import random
+import requests
+import threading
 import os
 import re
 
@@ -31,12 +34,35 @@ def calc(update, context):
 		y = "Invalid Input."
 	update.effective_message.reply_text(y)
 
+#cat spam function that sends a cat pic (taken from a free api using request.get) every 5 seconds using a global timer for every chat
+
+timers = {}
+
+def catto(update, context):
+	global timers
+	data = requests.get("https://api.thecatapi.com/v1/images/search").json()
+	update.effective_chat.send_photo(data[0]["url"])
+	if update.effective_chat.id in timers and timers[update.effective_chat.id].is_alive():
+		timers[update.effective_chat.id].cancel()
+	timers[update.effective_chat.id] = threading.Timer(2.0, catto, [update, context])
+	timers[update.effective_chat.id].start()
+
+def stop(update, context):
+	global timers
+	if update.effective_chat.id in timers and timers[update.effective_chat.id].is_alive():
+		timers[update.effective_chat.id].cancel()
+		del timers[update.effective_chat.id]        
+
 
 #command handlers
 
 startHandler = CommandHandler("start", start)
 calcHandler = CommandHandler("calc", calc)
+cattoHandler = CommandHandler("catto", catto)
+stopHandler = CommandHandler("stopcat", stop)
 dispatcher.add_handler(startHandler)   
 dispatcher.add_handler(calcHandler) 
+dispatcher.add_handler(cattoHandler)    
+dispatcher.add_handler(stopHandler)
 
 updater.start_polling()
