bb75f3a75f4094a15e5ef677d0fb994e23d0737e
Raghav <zCalibeRz1337@gmail.com>
2021-06-17 14:12:13 +0530

    Added a calculator function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A Telegram bot that does stuff.
 
 Start it for a 'cool' message!
+
+Use /calc to make it perform basic arithmetic!
--- c4libot.py
+++ c4libot.py
@@ -4,6 +4,7 @@
 from telegram.ext import MessageHandler, Filters, CommandHandler, Updater
 import logging
 import os
+import re
 
 #setting up and updater and dispatcher
 
@@ -20,9 +21,22 @@ logging.basicConfig(format = "%(asctime)s - %(name)s - %(levelname)s - %(message
 def start(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Hello, I am property of the mathgod caliber.")
 
+#calculator using regex
 
+def calc(update, context):
+	x = re.sub("[^0-9\+\-\*/\.\(\)]", "", "".join(context.args).replace("^", "**"))
+	try:
+		y = eval(x)
+	except:
+		y = "Invalid Input."
+	update.effective_message.reply_text(y)
+
+
+#command handlers
 
 startHandler = CommandHandler("start", start)
-dispatcher.add_handler(startHandler)    
+calcHandler = CommandHandler("calc", calc)
+dispatcher.add_handler(startHandler)   
+dispatcher.add_handler(calcHandler) 
 
 updater.start_polling()
