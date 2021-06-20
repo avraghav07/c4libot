97ddc606231c425b0ddb5ede55d7e3f5143d3bb4
Raghav <zCalibeRz1337@gmail.com>
2021-06-20 15:53:52 +0530

    Updated and optimized blackjack.py to not contain any command line stuff

--- Games/BlackJack.py
+++ Games/BlackJack.py
@@ -11,38 +11,45 @@ class RoundResult(Enum):
 	Loss = 3
 	Push = 4
 
+
 class Card:
+
 	suits = ["♣", "♦", "♥", "♠"]
 	ranks = range(1, 14)
 	rankText = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+
 	def __init__(self, suit, rank):
 		self.suit = suit
 		self.rank = rank
+
 	def toString(self):
 		return f"({self.rankText[self.rank - 1]} of {self.suit})"
 
 class Deck:
+
 	def __init__(self):
 		self.cards = []
-	def populate(self):
-		for suit in Card.suits:
-			for rank in Card.ranks:
-				card = Card(suit, rank)
-				self.cards.append(card)
+	def populate(self, decks):
+		self.cards += [Card(suit, rank) for x in range(decks) for suit in Card.suits for rank in Card.ranks]
 	def shuffle(self):
 		random.shuffle(self.cards)
 	def drawCard(self):
 		if (len(self.cards) == 0):
-			return False
+			self.populate(4)
+			self.shuffle()
 		return self.cards.pop()
 
 class Hand:
+
 	def __init__(self):
 		self.cards = []
+
 	def addCard(self, card):
 		self.cards.append(card)
+
 	def clear(self):
 		self.cards.clear()
+
 	def getValue(self):
 		numberOfAces = 0
 		handValue = 0
@@ -56,8 +63,10 @@ class Hand:
 			if (handValue > 21):
 				handValue -= 10
 		return handValue
+
 	def isBust(self):
 		return (self.getValue() > 21)
+
 	def toString(self):
 		handStrings = []
 		for card in self.cards:
@@ -65,63 +74,84 @@ class Hand:
 		return (", ".join(handStrings) + f", [{self.getValue()}]")
 
 class Player:
+
 	def __init__(self, ID, chips):
 		self.ID = ID
 		self.chips = chips
 		self.currentBet = 0
 		self.hand = Hand()
 		self.roundResult = RoundResult.NoResult
+
 	def placeBet(self, amount):
-		if (amount <= self.chips):
+		if (amount >= -self.currentBet and amount <= self.chips and amount != 0):
 			self.currentBet += amount
 			self.chips -= amount
 			return True
 		return False
+
 	def processResult(self):
+		self.hand.clear()
 		if (self.roundResult == RoundResult.Win):
 			self.chips += self.currentBet * 2
 		elif (self.roundResult == RoundResult.Push):
 			self.chips += self.currentBet
 		self.currentBet = 0
+		self.roundResult = RoundResult.NoResult
 
-class BlackjackGame:
-	def __init__(self):
-		self.players = []
-		self.currentPlayerIndex = 0
+class Game:
+
+	def __init__(self, message, starterUserID):
+		self.message = message
+		self.starterUserID = starterUserID
+		self.starterd = False
+		self.roundStarted = False
+		self.players = {}
+		self.currentPlayerIndex = -1
 		self.dealerHand = Hand()
 		self.deck = Deck()
-		self.deck.populate()
+
 	def startRound(self):
+		self.nextPlayer()
+		self.started = True
+		self.roundStarted = True
 		self.deck.shuffle()
 		for x in range(2):
 			self.dealHands()
+
 	def dealHands(self):
-		for player in self.players:
-			player.hand.addCard(self.deck.drawCard())
+		for userID, player in self.players.items():
+			if (player.currentBet > 0):
+				player.hand.addCard(self.deck.drawCard())
 		self.dealerHand.addCard(self.deck.drawCard())
+
 	def processDecision(self, decision):
 		if (decision == PlayerDecision.Hit):
-			player = self.players[self.currentPlayerIndex]
-			player.hand.addCard(self.deck.drawCard())
-			if (player.hand.isBust()):
-				player.roundResult = RoundResult.Loss
-				player.processResult()
-				self.nextPlayer()
-		elif (decision == PlayerDecision.Stay):
-			self.nextPlayer()
-	def nextPlayer(self):
+			self.currentPlayer.hand.addCard(self.deck.drawCard())
+			if (self.currentPlayer.hand.isBust()):
+				self.currentPlayer.roundResult = RoundResult.Loss
+				return self.nextPlayer()
+		elif (decision == PlayerDecision.Stand):
+			return self.nextPlayer()
+
+	def nextPlayer(self):		
 		self.currentPlayerIndex += 1
 		if (self.currentPlayerIndex == len(self.players)):
 			self.endRound()
+			return False
+		self.currentPlayer = self.players[list(self.players.keys())[self.currentPlayerIndex]]
+		if (self.currentPlayer.currentBet == 0):
+			return self.nextPlayer()
+		return True	
+
 	def endRound(self):
 		while (self.dealerHand.getValue() <= 16):
 			self.dealerHand.addCard(self.deck.drawCard())
 		dealerBust = self.dealerHand.isBust()
-		for player in self.players:
+		for userID, player in self.players.items():
 			if (dealerBust):
 				player.roundResult = RoundResult.Win
 			else:
-				if (player.hand.isBust()):
+				if (player.hand.isBust() or player.currentBet <= 0):
 					continue
 				playerHandValue = player.hand.getValue()
 				dealerHandValue = self.dealerHand.getValue()
@@ -132,54 +162,11 @@ class BlackjackGame:
 				else:
 					player.roundResult = RoundResult.Push
 			player.processResult()
-	def reset(self):
-		for player in self.players:
-			player.hand.clear()
-			player.roundResult = RoundResult.NoResult
+			
+	def processResult(self):
+		for userID, player in self.players.items():
+			player.processResult()
 		self.dealerHand.clear()
-		self.currentPlayerIndex = 0
+		self.currentPlayerIndex = -1
+		self.roundStarted = False
 
-game = BlackjackGame()
-gamer = Player(1234, 50)
-game.players.append(gamer)
-while (True):
-	try:
-		chips = int(input(f"You have {gamer.chips} chips. How many chips do you wish to bet? "))
-	except KeyboardInterrupt:
-		print("no")
-		quit()
-	except:
-		print("Invalid Input")
-		continue
-	x = gamer.placeBet(chips)
-	if x:
-		game.startRound()
-		print("The dealer's first card is " + game.dealerHand.cards[0].toString())
-		print("Your hand: " + gamer.hand.toString())
-		while (True):
-			choice = input("Do you want to hit or stay? ").lower()
-			if (choice == "hit"):
-				game.processDecision(PlayerDecision.Hit)
-				print("Your hand: " + gamer.hand.toString())
-				if (gamer.roundResult == RoundResult.Loss):
-					print("Your hand is bust!")
-					break
-			elif (choice == "stay"):
-				game.processDecision(PlayerDecision.Stay)
-				break
-			else:
-				print("Invalid Input")
-		print("Dealer's hand: " + game.dealerHand.toString())
-		if (gamer.roundResult == RoundResult.Win):
-			print("You Won!")
-		elif (gamer.roundResult == RoundResult.Loss):
-			print("You Lost!")
-		elif (gamer.roundResult == RoundResult.Push):
-			print("It's a Tie!")
-		if (gamer.chips == 0):
-			print("You're out of chips!")
-			quit()
-		print("You now have " + str(gamer.chips) + " chips")
-		game.reset()
-	else:
-		print("You do not have those many chips to bet")
